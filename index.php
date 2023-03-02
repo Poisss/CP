202f86afb06148e8a78b1ca7baf65ea3047a66ca
Poisss <averinartem2003@gmail.com>
2023-03-02 15:20:31 +0500

    frame

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Не знаешь где найти расписание? Это печально">
+		<meta name="keywords" content="Расписание мпк, Расписание, мпк">
+		<meta name="Author" content="">
+		<meta name="Copyright" content="">
+		<meta name="Address" content="мпк">
+		<link rel="shortcut icon" href="">
+        <title>CP</title>
+        <link rel="stylesheet" href="assets/css/css.css">
+        <script defer src="assets/js/js.js"></script>
+    </head>
+    <body>
+        <div id="app">
+            <div class="container">
+                <header>
+                    
+                </header>
+                <main>
+
+                </main>
+                <footer>
+
+                </footer>
+            </div>
+        </div>
+    </body>
+</html>
