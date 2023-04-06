c99e3db9565871be2b87c754ebe70792e8999cb3
student <student@vuz.magtu.ru>
2023-04-06 10:33:53 +0500

    vue project

---
+++ site/project/src/components/header.vue
@@ -0,0 +1,35 @@
+<script>
+
+</script>
+
+<template>
+    <div class="block">
+        <div class="header">
+            <div class="field">
+                1
+            </div>
+        </div>
+        <div class="main">
+            <div class="field">
+                1
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.block{
+    height: 100vh;
+    width: 100vw;
+    background: turquoise;
+}
+.header{
+    width: 100vw;
+    background: aquamarine;
+}
+.field{
+    max-width: 1200px;
+    padding: 20px;
+    margin: 0 auto;
+}
+</style>
