b4c3e3a97edc2fd6e8646d9b6e0bcf256d009a16
student <student@vuz.magtu.ru>
2023-06-24 13:18:02 +0500

    bag openingTimes first close

--- site/project/src/components/catalogOpeningTimes.vue
+++ site/project/src/components/catalogOpeningTimes.vue
@@ -14,6 +14,9 @@ export default {
             if (openingHours.opening === 'закрыто') {
                 return false;
             }
+            if (openingHours.opening === 'открыто') {
+                return true;
+            }
             const openingTime = new Date();
             const closingTime = new Date();
             const [openingHoursStart, openingHoursEnd] = openingHours.opening.split(':');
@@ -23,6 +26,10 @@ export default {
             openingTime.setMinutes(openingHoursEnd);
             closingTime.setHours(closingHoursStart);
             closingTime.setMinutes(closingHoursEnd);
+
+            if (closingTime < openingTime) {
+                closingTime.setDate(closingTime.getDate() + 1);
+            }
 
             return openingTime < new Date() && closingTime > new Date();
         },
@@ -38,21 +45,42 @@ export default {
 
             return openingHours.closing;
         },
+        nextDayOpeningTime(){
+            let nexDay=new Date();
+            nexDay.setDate(nexDay.getDate()+1);        
+            const nextDayIndex=nexDay.getDay()-1;
+            const openingHours=this.OpeningTimes[nextDayIndex>=0?nextDayIndex:6];       
+            return openingHours.opening;
+        },
+        nextDayName() {
+            let nextDay = new Date();
+            nextDay.setDate(nextDay.getDate() + 1);
+            const nextDayIndex = nextDay.getDay() - 1;
+            const openingHours = this.openingTimes[nextDayIndex >= 0 ? nextDayIndex : 6];
+            return openingHours.day;
+        },
     },
 }
 </script>
 <template>
     <div class="activity-block-main-time" v-if="isOpen">
-        <span class="activity-block-main-time-open">Открыто</span> до {{ closingTime }}
-    </div>
-    <div class="activity-block-main-time" v-else>
-        <div v-if="openingTime =='закрыто'">
-            <span class="activity-block-main-time-closed">Закрыто</span>
+        <div v-if="openingTime == 'открыто'">
+            <span class="activity-block-main-time-open">Открыто</span>
         </div>
         <div v-else>
+            <span class="activity-block-main-time-open">Открыто</span> до {{ closingTime }}
+        </div>
+    </div>
+    <div class="activity-block-main-time" v-else>
+        <div v-if="openingTime =='закрыто' && nextDayOpeningTime=='закрыто'">
+            <span class="activity-block-main-time-closed">Закрыто</span>
+        </div>
+        <div v-else-if="openingTime=='закрыто'">
             <span class="activity-block-main-time-closed">Закрыто</span>, откроется в {{ openingTime }}
         </div>
-        
+        <div v-else>
+            <span class="activity-block-main-time-closed">Закрыто</span>, откроется в {{ nextDayOpeningTime }}
+        </div>
     </div>
 </template>
 <style scoped>
